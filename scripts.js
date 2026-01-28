90f0a0f3cd57de29dd04209d655dcf4b53aa3017
Rodrigo Souza <rodsouzax@gmail.com>
2026-01-28 04:07:34 -0300

    feat: handle form submission and capture input elements

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -15,3 +18,7 @@ function formatCurrencyBRL(value) {
   });
   return value;
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
