8ea72fd261c4708bd8878f866c0a5dda1a0e97c2
Rodrigo Souza <rodsouzax@gmail.com>
2026-01-28 22:58:41 -0300

    feat: implement updateTotals function to track list count

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
 const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -67,8 +68,21 @@ function expenseAdd(newExpense) {
 
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
     expenseList.append(expenseItem);
+
+    updateTotals();
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.error(error);
+  }
+}
+
+function updateTotals() {
+  try {
+    const items = expenseList.children;
+
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`;
+  } catch (error) {
+    console.error(error);
+    alert("Não foi possível atualizar os totais de despesas.");
   }
 }
